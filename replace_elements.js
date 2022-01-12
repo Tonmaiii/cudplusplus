704c04fb624de3bcdf022e0d32f34aeb95ffb8cc
Tonmaiii <pruek.rodsawat@gmail.com>
2022-01-12 14:28:03 +0700

    refactor

--- replace_elements.js
+++ replace_elements.js
@@ -10,19 +10,37 @@ setInterval(() => {
         }
 
         if (element.tagName === 'SPAN' && !element.inverted) {
-            element.style.color &&
-                (element.style.color = invertColor(element.style.color))
-            element.style['background-color'] &&
-                (element.style['background-color'] = invertColor(
-                    element.style['background-color']
-                ))
+            invert(element)
             element.inverted = true
         }
     })
 })
 
-const defaultImageURL =
-    'https://oss-cudplus-own.s3.ap-southeast-1.amazonaws.com/FQsvhVlPNMbq8tdgCs0m_3_2020_05_03_033743/cud_plus_logo.svg?X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA5OEOOMBR3OZRLCT6%2F20220105%2Fap-southeast-1%2Fs3%2Faws4_request&X-Amz-Date=20220105T235240Z&X-Amz-SignedHeaders=host&X-Amz-Expires=600000&X-Amz-Signature=c6faf6bba5d5eae1cc062852b2d03248d9eb5a91c93c1be6bc2e27d1a6915116'
+const invert = element => {
+    element.style.color &&
+        (element.style.color = invertColor(element.style.color))
+    element.style['background-color'] &&
+        (element.style['background-color'] = invertColor(
+            element.style['background-color']
+        ))
+}
+
+const invertColor = inputColor => {
+    const color = getColor(inputColor)
+    color[0] = map(color[0], 0x0, 0xff, 0xff, 0x20)
+    color[1] = map(color[1], 0x0, 0xff, 0xff, 0x20)
+    color[2] = map(color[2], 0x0, 0xff, 0xff, 0x24)
+    return `rgba(${color})`
+}
+
+const map = (value, start1, stop1, start2, stop2) =>
+    ((value - start1) / (stop1 - start1)) * (stop2 - start2) + start2
+
+const getColor = color => {
+    if (color.match(/rgba?/)) return rgbaExpand(color)
+    else if (color.match(/#/)) return hexExpand(color)
+    else return hexExpand(cssColors[color])
+}
 
 const rgbaExpand = rgba =>
     rgba
@@ -37,25 +55,11 @@ const hexExpand = hex => {
         )
         .slice(1)
         .filter(n => n !== undefined)
-    values[3] = 0xff
+    values[3] === undefined && (values[3] = 'ff')
+    values.map(n => parseInt(n, 16))
+    values[3] /= 0xff
     return values
-        .map((n, i) => (i === 3 ? n / 0xff : parseInt(n, 16)))
-        .filter(n => n !== undefined)
 }
 
-const getColor = color => {
-    if (color.match(/rgba?/)) return rgbaExpand(color)
-    else if (color.match(/#/)) return hexExpand(color)
-    else return hexExpand(cssColors[color])
-}
-
-const invertColor = inputColor => {
-    const color = getColor(inputColor)
-    color[0] = map(color[0], 0x0, 0xff, 0xff, 0x20)
-    color[1] = map(color[1], 0x0, 0xff, 0xff, 0x20)
-    color[2] = map(color[2], 0x0, 0xff, 0xff, 0x24)
-    return `rgba(${color})`
-}
-
-const map = (value, start1, stop1, start2, stop2) =>
-    ((value - start1) / (stop1 - start1)) * (stop2 - start2) + start2
+const defaultImageURL =
+    'https://oss-cudplus-own.s3.ap-southeast-1.amazonaws.com/FQsvhVlPNMbq8tdgCs0m_3_2020_05_03_033743/cud_plus_logo.svg?X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA5OEOOMBR3OZRLCT6%2F20220105%2Fap-southeast-1%2Fs3%2Faws4_request&X-Amz-Date=20220105T235240Z&X-Amz-SignedHeaders=host&X-Amz-Expires=600000&X-Amz-Signature=c6faf6bba5d5eae1cc062852b2d03248d9eb5a91c93c1be6bc2e27d1a6915116'
